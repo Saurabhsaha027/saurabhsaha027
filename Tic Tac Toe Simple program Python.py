f5ea6398e60e65dd2987ce0ec347495b58a9e6c2
Saurabh Kumar <47217012+Saurabhsaha027@users.noreply.github.com>
2020-08-05 15:41:08 +0530

    Update Tic Tac Toe Simple program Python.py

--- Tic Tac Toe Simple program Python.py
+++ Tic Tac Toe Simple program Python.py
@@ -1,18 +1,18 @@
 import random
 import time
-def board(pName,pc,p):
-    print("\t\t\t  :  " + pName.upper() + "   vs   "+pc )
+def board(pName,pc,p,ps,cs):
+    print("\t\t\t  : ",pName.upper()+"["+ps+"]", "   vs   ",pc+"["+ps+"]" )
     print("\t\t\t  _____________________________________")
-    print("\t\t\t |           |            |            |")
-    print("\t\t\t |     "+p[0]+ "     |      "+p[1]+"     |      "+p[2]+"     |")
-    print("\t\t\t |           |            |            |")
-    print("\t\t\t  _____________________________________")
-    print("\t\t\t |           |            |            |")
-    print("\t\t\t |     "+p[3]+ "     |      "+p[4]+"     |      "+p[5]+"     |")
+    print("\t\t\t |1          |2           |3           |")
+    print("\t\t\t |     "+p[0]+ "     |      "+p[1]+"     |     "+p[2]+"      |")
     print("\t\t\t |           |            |            |")
     print("\t\t\t  _____________________________________")
+    print("\t\t\t |4          |5           |6           |")
+    print("\t\t\t |     "+p[3]+ "     |      "+p[4]+"     |     "+p[5]+"      |")
     print("\t\t\t |           |            |            |")
-    print("\t\t\t |     "+p[6]+ "     |      "+p[7]+"     |      "+p[8]+"     |")
+    print("\t\t\t  _____________________________________")
+    print("\t\t\t |7          |8           |9           |")
+    print("\t\t\t |     "+p[6]+ "     |      "+p[7]+"     |     "+p[8]+"      |")
     print("\t\t\t |           |            |            |")
     print("\t\t\t  -------------------------------------")
 def winner(p,t):
@@ -28,24 +28,23 @@ def winner(p,t):
     else:
         return False
 
-
+1
 if __name__ == '__main__':
     again=1
-    print('\n\n\n!!@saurabhsaha027-------------: PlAY  ("_") TIC TAC TOE ("_")  WITH COMPUTER/Friend :-----------!!')
+    print('\n\n\n!!-------------: PlAY  ("_") TIC TAC TOE ("_")  WITH COMPUTER / Friend :-----------!!')
     print(" `````````````````````````````````````````````````````````````````````````````")
-    print("---------: MODE :---------\n 1 - COMPUTER :\n 2 - FRIEND:\n ")
+    print("---------: MODE :---------\n 1   -    COMPUTER :\n 2   -    FRIEND: ")
     ck = input("Enter Choice : ")
     while (ck != "1" and ck != "2"):
         print("------> Invalid Choice <-----")
         ck = input("Enter Choice : ")
     ck = ord(ck) - 48
-
-    pName = input("Enter Your Name :\t")
+    pName = input("Enter Your Name :\t").upper()
     if (ck == 1):
         pc = "COMPUTER"
     else:
         pc = input("Enter Friend's Name : ").upper()
-    p = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    p = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     cnt = 0
     ps = input("Choose Your Symbol  (O , X) : ").upper()
     while (ps != "O" and ps != "X"):
@@ -54,8 +53,11 @@ if __name__ == '__main__':
         cs = "O"
     else:
         cs = "X"
-    board(pName, pc, p)
+
     while(again):
+        board(pName, pc, p,ps,cs)
+        print("\n\t\t GAME IS LOADING PLEASE WAIT......!!!!!")
+        time.sleep(3)
         while (cnt < 9):
             if (cnt % 2 == 0):
                 if (ck == 1):
@@ -79,22 +81,23 @@ if __name__ == '__main__':
                     if (ck == 1):
                         time.sleep(1)
                     print("\t", pc, "TURNS :\t DONE: ")
-                    board(pName, pc, p)
+                    board(pName, pc, p, ps, cs)
                     if (winner(p, cs)):
-                        print("  !!!---->-->--->opps, Bad Luck --Better Luck Next Time-------!!!")
+                        print("  !!!---->-->--->opps, Bad Luck -----Better Luck Next Time-------!!!")
+                        cnt=0
                         break
 
             else:
-                print(pName, "Turns ", end=" ")
-                choice = input("Turns Enter Choice :")
+                print(pName, "Turns ", end="")
+                choice = input("Enter Choice :")
                 choice = ord(choice) - 48
                 if (choice > 0 and choice < 10):
                     if (p[choice - 1] == cs or p[choice - 1] == ps):
-                        print("------>Invalid Choice<-----")
+                        print("------>Invalid Position :<-----")
                     else:
                         p[choice - 1] = ps
                         cnt += 1
-                        board(pName, pc, p)
+                        board(pName, pc, p, ps, cs)
                         if (winner(p, ps)):
                             print("    !!!->-->->-Congrats " + pName + ", YOU WON<--<-<-  !!!-------!!!")
                             break;
@@ -102,7 +105,7 @@ if __name__ == '__main__':
                     print("------>Invalid Choice<-----")
         if (cnt > 8):
             print("   ----->>-->  No-One Wons ,<--<<-------")
-        print("Wanna play again: type 'yes': Otherwise click any key Except Enter")
+        print("Wanna play again: type 'yes': Otherwise click any key ::",end="")
         sk=input().upper()
         if(sk!="YES"):
             again=0
